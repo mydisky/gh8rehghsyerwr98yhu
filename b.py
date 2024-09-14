6568546577b456c1963941b888d52ff3892f9f86
mydisky <133907064+mydisky@users.noreply.github.com>
2024-09-14 22:57:37 +0545

    Update b.py

--- b.py
+++ b.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.environ.get("6730567676:AAFfMaZCIbPUj2X9T7ZdVWsFtwlwRd3oN14")
 DB_URI = os.environ.get("mongodb+srv://aaroha:aaroha@cluster0.6jc4x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DB_URI = os.environ.get("DB_URI")
-client = TelegramClient('ShortUrlLink', APP_ID=os.environ.get("21159773"), API_HASH=os.environ.get("49ae08543a07335e195756eba2f56e11")).start(bot_token=BOT_TOKEN)
+client = TelegramClient('ShortUrlLink', APP_ID = os.environ.get("21159773"), API_HASH = os.environ.get("49ae08543a07335e195756eba2f56e11")).start(bot_token=BOT_TOKEN)
 
 from f import *
 start_message = """<b>
